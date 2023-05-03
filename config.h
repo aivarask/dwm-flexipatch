8f09b342bc3378da0b7c15cb27454ac019f01289
Aivaras Kalesnykas <kalesnykas.aivaras@gmail.com>
2023-05-03 18:26:07 +0300

    floating tilda

--- config.h
+++ config.h
@@ -530,12 +530,13 @@ static const Rule rules[] = {
                 RULE(.wintype = WTYPE "SPLASH", .isfloating = 1)
                     RULE(.class = "Gimp", .tags = 1 << 4)
                         RULE(.class = "Firefox", .tags = 1 << 7)
+                            RULE(.class = "Tilda", .isfloating = 1)
 #if RENAMED_SCRATCHPADS_PATCH
-                            RULE(.instance = "spterm", .scratchkey = 's',
-                                 .isfloating = 1)
+                                RULE(.instance = "spterm", .scratchkey = 's',
+                                     .isfloating = 1)
 #elif SCRATCHPADS_PATCH
-                            RULE(.instance = "spterm", .tags = SPTAG(0),
-                                 .isfloating = 1)
+                                RULE(.instance = "spterm", .tags = SPTAG(0),
+                                     .isfloating = 1)
 #endif // SCRATCHPADS_PATCH
 };
 
